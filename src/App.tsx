00a0ef95a8167aaedc78d59ad4470dc0bf31272d
Kirill Tokarev <kirill.tokarev@osinit.com>
2022-10-05 19:58:55 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css'
+
+function App () {
+  return <div>TEST CICD V5</div>
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
+import App from './App'
+import './index.css'
+
+const rootElement = document.getElementById('root')
+
+if (!rootElement) throw new Error('Failed to find the root element')
+
+const root = createRoot(rootElement)
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
